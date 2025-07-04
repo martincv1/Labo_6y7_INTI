4ca01099ed8b4f4d84f4c7101c1d2b137b85266a
Martín Castelli <martincastelliv@gmail.com>
2025-07-04 16:00:48 -0300

    Cambios menores a analisis_tiempo

--- Calibracion_SLM/scripts/analisis/analisis_tiempo.py
+++ Calibracion_SLM/scripts/analisis/analisis_tiempo.py
@@ -58,7 +58,7 @@ def extraer_datos(nombre_archivo):
     return None, None, None, None  
 
 
-dir_path = '/home/lorenzo/Labo_6y7_INTI/Calibracion_SLM/data/fase_tiempo_rapido'
+dir_path = '/home/lorenzo/Labo_6y7_INTI/Calibracion_SLM/data/fase_tiempo'
 paths = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith('.pkl')]
 #paths = []
 #for archivo in os.listdir(dir_path):
@@ -68,8 +68,8 @@ paths = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith('
 
 data = {
     40: {"dts": [], "fase_nm": [], "fase_m": []},
-    128: {"dts": [], "fase_nm": [], "fase_m": []},
-    230: {"dts": [], "fase_nm": [], "fase_m": []}
+    120: {"dts": [], "fase_nm": [], "fase_m": []},
+    200: {"dts": [], "fase_nm": [], "fase_m": []}
     }
 
 for p in paths:
@@ -79,59 +79,87 @@ for p in paths:
         data[intensidad]["fase_nm"].append(fase_nm)
         data[intensidad]["fase_m"].append(fase_m)
 
+#dfs = {}
+#for intensidad, valores in data.items():
+#    if valores["dts"]:  # Si hay datos para esta intensidad
+#        dfs[intensidad] = pd.DataFrame({
+#            "Hora": valores["dts"],
+#            "Fase_no_modulada": valores["fase_nm"],
+#            "Fase_modulada": valores["fase_m"]
+#        })
+
 dfs = {}
 for intensidad, valores in data.items():
     if valores["dts"]:  # Si hay datos para esta intensidad
-        dfs[intensidad] = pd.DataFrame({
+        # Crear DataFrame temporal
+        temp_df = pd.DataFrame({
             "Hora": valores["dts"],
             "Fase_no_modulada": valores["fase_nm"],
             "Fase_modulada": valores["fase_m"]
         })
+        # Ordenar por hora
+        temp_df = temp_df.sort_values("Hora")
+        # Eliminar duplicados (manteniendo el primero que aparece)
+        #temp_df = temp_df.drop_duplicates(subset="Hora", keep='first')
+        # Resetear índice
+        dfs[intensidad] = temp_df.reset_index(drop=True)
 
 complejos_40 = np.exp(1j*dfs[40]["Fase_no_modulada"])
 complejos_40_mod = np.exp(1j*dfs[40]["Fase_modulada"])
 div_40 = complejos_40 / complejos_40_mod
-fase_40 = np.unwrap(np.angle(div_40))
+fase_40 = np.angle(div_40)
 
-complejos_128 = np.exp(1j*dfs[128]["Fase_no_modulada"])
-complejos_128_mod = np.exp(1j*dfs[128]["Fase_modulada"])
-div_128 = complejos_128/complejos_128_mod
-fase_128 = np.unwrap(np.angle(div_128))
 
-complejos_230 = np.exp(1j*dfs[230]["Fase_no_modulada"])
-complejos_230_mod = np.exp(1j*dfs[230]["Fase_modulada"])
-div_230 = complejos_230/complejos_230_mod
-fase_230 = np.unwrap(np.angle(div_230))
+mask = fase_40 > 0
+fase_40_limpia = fase_40[mask]
+
+
+
+complejos_120 = np.exp(1j*dfs[120]["Fase_no_modulada"])
+complejos_120_mod = np.exp(1j*dfs[120]["Fase_modulada"])
+div_120 = complejos_120/complejos_120_mod
+fase_120 = np.angle(div_120)
+
+complejos_200 = np.exp(1j*dfs[200]["Fase_no_modulada"])
+complejos_200_mod = np.exp(1j*dfs[200]["Fase_modulada"])
+div_200 = complejos_200/complejos_200_mod
+fase_200 = np.angle(div_200)
 
 #defino mi array de tiempos a partir de data.datatime
 horas_segundos_40 = [h.hour * 3600 + h.minute * 60 + h.second for h in dfs[40]["Hora"]]
-horas_segundos_128 = [h.hour * 3600 + h.minute * 60 + h.second for h in dfs[128]["Hora"]]
-horas_segundos_230 = [h.hour * 3600 + h.minute * 60 + h.second for h in dfs[230]["Hora"]]
+horas_segundos_120 = [h.hour * 3600 + h.minute * 60 + h.second for h in dfs[120]["Hora"]]
+horas_segundos_200 = [h.hour * 3600 + h.minute * 60 + h.second for h in dfs[200]["Hora"]]
 
-#plt.scatter(horas_segundos_40, dfs[40]["Fase_modulada"])
-#plt.xlabel("Tiempo [s]")
-#plt.ylabel("Fase [rad]")
-#plt.title("Fase_modulada, I=40")
-#plt.show()
-#plt.scatter(horas_segundos_40, dfs[40]["Fase_no_modulada"])
-#plt.xlabel("Tiempo [s]")
-#plt.ylabel("Diferencia de Fase [rad]")
-#plt.title("Fase no modulada, I= 40")
-#plt.show()
-plt.scatter(horas_segundos_40, fase_40)
+horas_segundos_200 = np.array(horas_segundos_200)
+tiempo_centrado_200 = horas_segundos_200 - horas_segundos_200.min()
+horas_segundos_40 = np.array(horas_segundos_40)
+tiempo_centrado_40 = horas_segundos_40 - horas_segundos_40.min()
+horas_segundos_120 = np.array(horas_segundos_120)
+tiempo_centrado_120 = horas_segundos_120 - horas_segundos_120.min()
+horas_segundos_40_filtrado = np.array(horas_segundos_40)[mask]
+
+plt.scatter(tiempo_centrado_40, fase_40)
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Diferencia de Fase [rad]")
 plt.title("Intensidad 40")
-#plt.plot(horas_segundos_120, dfs[120]["Fase_modulada"] - dfs[120]["Fase_no_modulada"])
-#plt.plot(horas_segundos_200, dfs[200]["Fase_modulada"] - dfs[200]["Fase_no_modulada"])
 plt.show()
-plt.scatter(horas_segundos_128, fase_128)
+#plt.scatter(horas_segundos_40_filtrado, fase_40_limpia)
+#plt.axhline(np.mean(fase_40_limpia), color='red', linestyle='--', label='Media')
+#plt.xlabel("Tiempo [s]")
+#plt.ylabel("Diferencia de Fase [rad]")
+#plt.title("Intensidad 40")
+#plt.legend()
+#plt.show()
+plt.scatter(tiempo_centrado_120, fase_120)
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Diferencia de Fase [rad]")
-plt.title("Intensidad 128")
+plt.title("Intensidad 120")
 plt.show()
-plt.scatter(horas_segundos_230, fase_230)
+plt.scatter(tiempo_centrado_200, fase_200)
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Diferencia de Fase [rad]")
-plt.title("Intensidad 230")
+plt.title("Intensidad 200")
 plt.show()
+
+print(dfs[40]["Fase_no_modulada"], type(dfs[40]["Fase_no_modulada"]))
+print(dfs[40]["Hora"])
