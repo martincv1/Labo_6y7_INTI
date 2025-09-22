7644a6203f4cfec7ed8279e9b680ea4c05ccbf97
j4517 <juan-marino@live.com.ar>
2025-09-22 11:33:22 -0300

    Creamos get_multiple_frame en clase jai_camera y probamos en stream_jai, cambios minimos a adquisicion_tension

--- Calibracion_SLM/scripts/adquisicion_y_control/adquisicion_tension.py
+++ Calibracion_SLM/scripts/adquisicion_y_control/adquisicion_tension.py
@@ -7,14 +7,14 @@ import pickle
 import os
 import cv2
 
-from Calibracion_SLM.scripts.adquisicion_y_control.acquisition_tools import jai_camera
-import Calibracion_SLM.scripts.adquisicion_y_control.slm_tools as slm_tools
+from acquisition_tools import jai_camera
+import slm_tools as slm_tools
 
 camera = jai_camera()
 
 # Defino array de intensidades e inicializo el SLM
 
-intensidades_array = np.arange(0, 255, 1)
+intensidades_array = np.arange(0, 255, 10)
 # Init HOLOEYE SLM Display SDK and make sure to check for the correct version this script was written with:
 err = HEDS.SDK.Init(4, 0)
 assert err == HEDSERR_NoError, HEDS.SDK.ErrorString(err)
@@ -26,13 +26,13 @@ assert slm.errorCode() == HEDSERR_NoError, HEDS.SDK.ErrorString(slm.errorCode())
 # Defino variables para realizar la medición
 
 resol_SLM = (1080, 1920)
-tiempo_espera_inicial = 120
-print(f'Espero {tiempo_espera_inicial} s antes de empezar')
+tiempo_espera_inicial = 0
+print(f"Espero {tiempo_espera_inicial} s antes de empezar")
 time.sleep(tiempo_espera_inicial)
-fecha = '1306'
+fecha = "2109"
 T_dia = 21
-cant_promedio = 5
-save_dir = r"data\fase_tension"
+cant_promedio = 1
+save_dir = r"data\fase_tension_prueba_camara"
 cant_pruebas_retrieve = 4
 tiempo_prueba = 0.05
 
@@ -42,11 +42,13 @@ os.makedirs(save_dir, exist_ok=True)
 # que promedio. Todo esto para un valor de tensión que cambio desde el PCM
 
 for i in intensidades_array:
-    print(f"Mostrando patrón con intensidad: {i}")    
+    print(f"Mostrando patrón con intensidad: {i}")
     patron = slm_tools.crear_patron(resol_SLM, "horizontal", "sup", i)
     # va a guardar cada patron como archivo.npy en la carpeta patrones
     # np.save(f"patrones/patron_{idx:03d}.npy", patron)
-    err, dataHandle = slm.loadImageData(patron)  # carga la data (array) a la video memory del display SLM
+    err, dataHandle = slm.loadImageData(
+        patron
+    )  # carga la data (array) a la video memory del display SLM
     assert err == HEDSERR_NoError, HEDS.SDK.ErrorString(err)
     err = dataHandle.show()  # Show the returned data handle on the SLM
     assert err == HEDSERR_NoError, HEDS.SDK.ErrorString(err)
@@ -54,13 +56,13 @@ for i in intensidades_array:
 
     camera.reset_queue()
 
-    file_name = os.path.join(save_dir, f'{fecha}_I{i}_T{T_dia}')
+    file_name = os.path.join(save_dir, f"{fecha}_I{i}_T{T_dia}")
     if cant_promedio == 1:
         frame = camera.get_frame()
-        cv2.imwrite(f'{file_name}.png', frame)
+        cv2.imwrite(f"{file_name}.png", frame)
     else:
         frame = camera.get_average_frame(cant_promedio)
-        with open(f'{file_name}.pkl', 'wb') as f:
+        with open(f"{file_name}.pkl", "wb") as f:
             pickle.dump(frame, f)
 
 camera.close()
--- Calibracion_SLM/scripts/adquisicion_y_control/stream_JAI.py
+++ Calibracion_SLM/scripts/adquisicion_y_control/stream_JAI.py
@@ -1,6 +1,7 @@
 # Importo librerias
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 from acquisition_tools import jai_camera
 
@@ -12,15 +13,26 @@ camera = jai_camera(buffers=BUFFER_COUNT)
 print("Camera ready")
 flag = True
 while flag:
-    frame = camera.get_frame()
+    frame1, frame2 = camera.get_multiple_frame(2)
 
-    if isinstance(frame, np.ndarray):
+    if isinstance(frame1, np.ndarray):
+        flag = False
+    if isinstance(frame2, np.ndarray):
         flag = False
 
-    print(f" {camera.frame_rate_val:.1f} FPS  {camera.bandwidth_val / 1000000.0:.1f} Mb/s     ", end='\r')
+    print(
+        f" {camera.frame_rate_val:.1f} FPS  {camera.bandwidth_val / 1000000.0:.1f} Mb/s     "
+    )
 
+frame1 = frame1.astype(np.float32)
+frame2 = frame2.astype(np.float32)
+mean_diff = np.mean(np.abs(frame1 - frame2))
+print(mean_diff)
+plt.imshow(frame1 - frame2)
+plt.show()
 camera.close()
 
-cv2.imshow("foto", frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("foto1", frame1)
+# cv2.imshow("foto2", frame2)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
